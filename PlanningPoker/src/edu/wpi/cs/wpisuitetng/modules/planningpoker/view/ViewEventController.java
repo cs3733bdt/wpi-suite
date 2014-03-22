4093d644e20ba261a14831b4072acb5c438631d7
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-03-22 19:01:03 -0400

    Created a controller object for the GUI

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/ViewEventController.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view;
+
+/**
+ * @author jonathanleitschuh
+ *
+ */
+public class ViewEventController {
+	private static ViewEventController instance = null;
+	private MainView main = null;
+	private ToolbarView toolbar = null;
+	
+	/**
+	 * Default constructor for the ViewEventController. Is protected to prevent instaniation.
+	 */
+	private ViewEventController(){}
+	
+	/**
+	 * Returns the single instance of the ViewEcentController.
+
+	 * @return the instance of this controller.
+	 */
+	public static ViewEventController getInstance(){
+		if(instance == null){
+			instance = new ViewEventController();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Sets the main view to the given view.
+
+	 * @param mainview The MainView
+	 */
+	public void setMainView(MainView mainview){
+		main = mainview;
+	}
+	
+	/**
+	 * Sets the ToolBar view to the given toolbar
+
+	 * @param tb The toolbar to be set as active.
+	 */
+	public void setToolBar(ToolbarView tb){
+		toolbar = tb;
+		toolbar.repaint(); //Unsure why this is necessary but this is what is done in Requirement Manager
+	}
+
+}
