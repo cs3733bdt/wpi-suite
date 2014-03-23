b271ebdaa3fcda339561b3a1b7a4b1e836dcc1d6
dstapply <dstapply@wpi.edu>
2014-03-23 17:30:10 -0400

    Revert "Dalton Tapply, Peter Leondires, Jacob McSweeney"
    This reverts commit a64e20a4ee1cdff7cfcfccd7ead12584f1674248.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/Game.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/Game.java
@@ -1,8 +1,6 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.models;
 
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -21,48 +19,21 @@ public class Game extends AbstractModel{
 	
 	private boolean hasTimeLimit;
 	
-	private Date creationTime;
+	private Calendar creationTime;
 	
 	//TODO find out how to implement existing module classes
 	private List<Requirement> requirements;
-	
-	/*
-	 * dstapply
-	 * 
-	 * We probably want to keep track of:
-	 * 	game creator
-	 * 	list of game players
-	 * 	moderator
-	 * 	list of estimates players can choose
-	 * 	player-estimate pairs
-	 * 	active?
-	 * 
-	 */
-	
-	/**
-	 * Constructs a Game with given id
-	 * @param id
-	 * @param hasTimeLimit checks if game has a time limit
-	 * 
-	 * @author dstapply
-	 */
-	public Game(int id, boolean hasTimeLimit) {
-		this.id = id;
-		this.hasTimeLimit = hasTimeLimit;
-		creationTime = new Date();
-	}
-	
-	/**
-	 * Gets the creating time and date of the game
-	 * @return a Formated Date String
-	 * 
-	 * @author dstapply
-	 */
-	public String getCreationTime() {
-		// Format the date-time stamp
-		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yy hh:mm a");
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
 		
-		return dateFormat.format(creationTime);
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	/**
@@ -73,36 +44,6 @@ public class Game extends AbstractModel{
 	public String toJSON() {
 		return new Gson().toJson(this, Game.class);
 	}
-	
-	/**
-	 * Returns an instance of Game constructed using the given
-	 * Game encoded as a JSON string.
-	 * 
-	 * @param json the json-encoded Game to deserialize
-	 * @return the Game contained in the given JSON
-	 */
-	public static Game fromJSON(String json) {
-	    final Gson parser = new Gson();
-	    return parser.fromJson(json, Game.class);
-	}
-
-	/**
-	 * Returns an array of Game parsed from the given JSON-encoded
-	 * string.
-	 * 
-	 * @param json a string containing a JSON-encoded array of Game
-	 * @return an array of Game deserialzied from the given json string
-	 */
-	public static Game[] fromJsonArray(String json) {
-	    final Gson parser = new Gson();
-	    return parser.fromJson(json, Game[].class);
-	}
-	
-	@Override
-	public void save() {}
-
-	@Override
-	public void delete() {}
 
 	@Override
 	public Boolean identify(Object o) {
