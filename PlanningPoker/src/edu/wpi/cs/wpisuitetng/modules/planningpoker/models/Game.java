0777a831451ae2c4d10a725b7d80651fb6f39966
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-03-23 15:04:19 -0400

    Added build dependency for the Requirement Manager. Began adding more data to the to the Game Model.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/Game.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/Game.java
@@ -1,6 +1,12 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.models;
 
+import java.util.Calendar;
+import java.util.List;
+
+import com.google.gson.Gson;
+
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 
 /**
  * Basic Game class that contains the data to be store for a Game
@@ -13,8 +19,10 @@ public class Game extends AbstractModel{
 	
 	private boolean hasTimeLimit;
 	
+	private Calendar creationTime;
+	
 	//TODO find out how to implement existing module classes
-	//private List<Requirement> requirements;
+	private List<Requirement> requirements;
 
 	@Override
 	public void save() {
@@ -28,16 +36,28 @@ public class Game extends AbstractModel{
 		
 	}
 
+	/**
+	 * Method toJSON.
+	 * @return String * @see edu.wpi.cs.wpisuitetng.modules.Model#toJSON() * @see edu.wpi.cs.wpisuitetng.modules.Model#toJSON()
+	 */
 	@Override
 	public String toJSON() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Gson().toJson(this, Game.class);
 	}
 
 	@Override
 	public Boolean identify(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		if(o == null){
+			return false;
+		}
+		if(o.getClass() != this.getClass()){
+			return false;
+		}
+		Game comp = (Game)o;
+		if(id != comp.id){
+			return false;
+		}
+		return true;
 	}
 
 }
