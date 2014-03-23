b95f4b08d401e66e786c0a8e0047fe4b6b1d1c02
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-03-23 15:43:18 -0400

    Switched the main view to a tabbed panel

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/MainView.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/MainView.java
@@ -8,13 +8,14 @@ import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 /**
  * This class sets the main view when the user goes to the PlanningPoker tab 
  * @author jonathanleitschuh
  *
  */
-public class MainView extends JPanel {
+public class MainView extends JTabbedPane {
 	
 	/**
 	 * Adds Main View of the planning poker panel when the user goes to the planning poker tab
