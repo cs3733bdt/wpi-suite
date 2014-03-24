dcd40e319e39ff558a36af8694413d4a08e5a30b
AndrewBusch <andrew.b.busch@gmail.com>
2014-03-24 16:11:52 -0400

    Added Get classes to Controller skeleton Andrew Busch
     On branch PPFeatureController
     Changes to be committed:
    	new file:   PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GetGameController.java
    	new file:   PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GetGameRequestObserver.java

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GetGameController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Andrew Busch
+ *
+ */
+public class GetGameController implements ActionListener {
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
