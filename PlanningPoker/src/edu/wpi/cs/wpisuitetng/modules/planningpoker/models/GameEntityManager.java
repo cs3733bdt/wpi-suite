0e5f6fad2534ddf765ed323962d92a2e85d93f59
AndrewBusch <andrew.b.busch@gmail.com>
2014-03-25 18:30:56 -0400

    finish filling GameEntityManager.java from 'step by step guide to creating a module' AndrewBusch
     On branch PPSaveGame
     Changes to be committed:
    	modified:   Core/WPISuite/src/edu/wpi/cs/wpisuitetng/ManagerLayer.java
    	modified:   PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/GameEntityManager.java

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/GameEntityManager.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/GameEntityManager.java
@@ -3,6 +3,8 @@
  */
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.models;
 
+import java.util.List;
+
 import edu.wpi.cs.wpisuitetng.Session;
 import edu.wpi.cs.wpisuitetng.database.Data;
 import edu.wpi.cs.wpisuitetng.exceptions.BadRequestException;
@@ -10,30 +12,31 @@ import edu.wpi.cs.wpisuitetng.exceptions.ConflictException;
 import edu.wpi.cs.wpisuitetng.exceptions.NotFoundException;
 import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 import edu.wpi.cs.wpisuitetng.modules.EntityManager;
+import edu.wpi.cs.wpisuitetng.modules.Model;
 
 /**
  * @author dstapply
- *
+ * 
  */
-public class GameEntityManager implements EntityManager<Game>{
+public class GameEntityManager implements EntityManager<Game> {
 
 	private Data db;
 
 	public GameEntityManager(Data db) {
 		this.db = db;
 	}
-	
+
 	@Override
 	public Game makeEntity(Session s, String content)
 			throws BadRequestException, ConflictException, WPISuiteException {
 		// TODO Auto-generated method stub
-		
+
 		final Game newGame = Game.fromJSON(content);
 
 		if (!db.save(newGame, s.getProject())) {
-	        throw new WPISuiteException();
-	    }
-		
+			throw new WPISuiteException();
+		}
+
 		return newGame;
 	}
 
@@ -41,25 +44,33 @@ public class GameEntityManager implements EntityManager<Game>{
 	public Game[] getEntity(Session s, String id) throws NotFoundException,
 			WPISuiteException {
 		// TODO Auto-generated method stub
-		return null;
+	    throw new WPISuiteException();
 	}
 
 	@Override
 	public Game[] getAll(Session s) throws WPISuiteException {
-		// TODO Auto-generated method stub
-		return null;
+
+		// Ask the database to retrieve all objects of the type
+		// PostBoardMessage.
+		// Passing a dummy PostBoardMessage lets the db know what type of object
+		// to retrieve
+		// Passing the project makes it only get messages from that project
+		List<Model> messages = db.retrieveAll(new Game(-1, false), s.getProject());
+
+		// Return the list of messages as an array
+		return messages.toArray(new Game[0]);
 	}
 
 	@Override
 	public Game update(Session s, String content) throws WPISuiteException {
 		// TODO Auto-generated method stub
-		return null;
+	    throw new WPISuiteException();
 	}
 
 	@Override
 	public void save(Session s, Game model) throws WPISuiteException {
-		// TODO Auto-generated method stub
-		
+	    // Save the given defect in the database
+	    db.save(model);
 	}
 
 	@Override
@@ -78,13 +89,13 @@ public class GameEntityManager implements EntityManager<Game>{
 	@Override
 	public void deleteAll(Session s) throws WPISuiteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public int Count() throws WPISuiteException {
 		// TODO Auto-generated method stub
-		return 0;
+	    return db.retrieveAll(new Game(-1, true)).size();
 	}
 
 	@Override
@@ -100,5 +111,5 @@ public class GameEntityManager implements EntityManager<Game>{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 }
