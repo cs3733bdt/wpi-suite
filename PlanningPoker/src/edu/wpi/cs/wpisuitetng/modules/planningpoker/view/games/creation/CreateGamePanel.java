7bfce3650ceb492fdfe81a91b751ae7e0e044753
Jeffrey Signore <jssignore@wpi.edu>
2014-03-31 17:13:27 -0400

    Changed the code structure so the requirement section of the active games panel is now in ActiveGamePanel instead of CreateGamelPanel

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/games/creation/CreateGamePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/games/creation/CreateGamePanel.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -71,6 +72,7 @@ public class CreateGamePanel extends JPanel {
 		JPanel nameAndDescPanel = new JPanel();
 		//nameAndDescPanel.setLayout(new GridLayout(2,2));
 		nameAndDescPanel.setLayout(new GridBagLayout());
+		c.weighty = .5;
 		c.gridx = 0;
 		c.gridy = 0;
 		c.gridwidth = 6;
@@ -107,7 +109,6 @@ public class CreateGamePanel extends JPanel {
 		c.gridx = 0;
 		c.gridy = 1;
 		c.gridwidth = 6;
-		c.insets = new Insets(0, 125, 0 ,0);
 		rightPanel.add(estimateTypePanel, c);
 		
 		JRadioButton cardsButton = new JRadioButton("Estimate With Cards");
@@ -116,185 +117,27 @@ public class CreateGamePanel extends JPanel {
 		JRadioButton textEntryButton = new JRadioButton("Estimate With Text Entry");
 		estimateTypePanel.add(textEntryButton);
 		
-		JPanel createReqPanel = new JPanel();
-		createReqPanel.setLayout(new GridBagLayout());
-		c.insets = new Insets(0, 0, 0 ,0);
+		ButtonGroup radioGroup = new ButtonGroup();
+        radioGroup.add(cardsButton);
+        radioGroup.add(textEntryButton);
+		
+        c.insets = new Insets(0, 0, 0 ,0);
 		c.ipadx = 0;
-		c.gridwidth = 2;
-		c.gridx = 0;
-		c.gridy = 2;
-		rightPanel.add(createReqPanel, c);
-		
-		JPanel blankPanel = new JPanel();
-		blankPanel.setMinimumSize(new Dimension(310, 200));
-		c.gridx = 1;
-		c.gridy = 2;
-		c.gridwidth = 2;
-		rightPanel.add(blankPanel, c);
-		
-		JPanel importReqPanel = new JPanel();
-		importReqPanel.setLayout(new GridBagLayout());
-		c.gridx = 4;
-		c.gridy = 2;
-		rightPanel.add(importReqPanel, c);
-		
-		JPanel currentReqsPanel = new JPanel();
-		currentReqsPanel.setLayout(new GridBagLayout());
-		c.gridx = 0;
-		c.gridy = 3;
 		c.gridwidth = 6;
-		rightPanel.add(currentReqsPanel, c);
-		
-		JLabel createReq = new JLabel("Create Requirement");
-		c.gridx = 0;
-		c.gridy = 0;
-		c.gridwidth = 2;
-		c.anchor = GridBagConstraints.CENTER;
-		createReqPanel.add(createReq, c);
-		
-		JLabel reqName = new JLabel("Name:");
-		c.gridwidth = 1;
-		c.gridx = 0;
-		c.gridy = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		createReqPanel.add(reqName, c);
-		
-		nameArea.setText("test name");
-		c.gridx = 1;
-		c.gridy = 1;
-		createReqPanel.add(nameArea, c);
-		
-		JLabel reqDesc = new JLabel("Description:");
-		c.gridx = 0;
-		c.gridy = 2;
-		c.anchor = GridBagConstraints.CENTER;
-		createReqPanel.add(reqDesc, c);
-		
-		descArea.setText("As a ______________ \nI would like to _______________________ \nso that ________________________________.");
-		descArea.setMinimumSize(new Dimension(200, 80));
-		descArea.setMaximumSize(new Dimension(200, 80));
-		c.gridwidth = 2;
-		c.gridheight = 3;
+		c.gridheight = 6;
 		c.gridx = 0;
 		c.gridy = 3;
+		c.weighty = .5;
 		
-		createReqPanel.add(descArea, c);
-		JButton addReqButton = new JButton("Add Requirement");
-		c.gridx = 0;
-		c.gridwidth = 2;
+		add(new AddRequirementsPanel(), c);
+		
+		c.gridwidth = 6;
 		c.gridheight = 1;
-		c.gridy = 6;
-		c.ipady = 0;
-		createReqPanel.add(addReqButton, c);
-		
-		JLabel importReq = new JLabel("Import Requirement");
-		c.gridwidth = 2;
-		c.gridx = 2;
-		c.gridy = 0;
-		c.anchor = GridBagConstraints.CENTER;
-		importReqPanel.add(importReq, c);
-		
-		/*
-		 * Initializes a table's columns and rows and the table
-		 */
-		String[] columnNames = {"Requirement"};
-		Object[][] data = {};
-		ActiveGamesTable table = new ActiveGamesTable(data, columnNames);
-		/*
-		 * Adds temporary data into the table. 
-		 * DELETE THIS ONCE DATA IS SUCCESSFULLY IMPORTED FROM REQUIREMENT MANAGER!!!!!!!!!!!!
-		 */
-		table.tableModel.addRow(new Object[]{"Requirement1"});
-		table.tableModel.addRow(new Object[]{"Requirement2"});
-		table.tableModel.addRow(new Object[]{"Requirement3"});
-		table.tableModel.addRow(new Object[]{"Requirement4"});
-		table.tableModel.addRow(new Object[]{"Requirement5"});
-		table.tableModel.addRow(new Object[]{"Requirement6"});
-		table.tableModel.addRow(new Object[]{"Requirement7"});
-		table.tableModel.addRow(new Object[]{"Requirement8"});
-		table.tableModel.addRow(new Object[]{"Requirement9"});
-		table.tableModel.addRow(new Object[]{"Requirement10"});		
-		/*
-		 * Puts the table within a scroll pane, and adds to the view.
-		 */
-		JScrollPane tablePanel = new JScrollPane(table);
-		tablePanel.setMinimumSize(new Dimension(200, 110));
-		tablePanel.setMaximumSize(new Dimension(200, 110));
-		c.gridwidth = 2;
-		c.gridx = 2;
-		c.gridy = 1;
-		importReqPanel.add(tablePanel, c);
-		
-		JButton importReqButton = new JButton("Import Requirement");
-		c.gridx = 2;
-		c.gridwidth = 2;
-		c.gridheight = 1;
-		c.gridy = 6;
-		c.ipady = 0;
-		c.ipadx = 0;
-		importReqPanel.add(importReqButton, c);
-		
-		JLabel currentReqs = new JLabel("Current Requirements");
-		c.gridx = 2;
-		c.gridy = 0;
-		c.gridwidth = 1;
-		c.anchor = GridBagConstraints.CENTER;
-		currentReqsPanel.add(currentReqs, c);
-		
-		
-		String[] columnNames2 = {"Requirement", "Description"};
-		Object[][] data2 = {};
-		ActiveGamesTable table2 = new ActiveGamesTable(data2, columnNames2);
-		/*
-		 * Adds temporary data into the table. 
-		 * DELETE THIS ONCE DATA IS SUCCESSFULLY IMPORTED FROM REQUIREMENT MANAGER!!!!!!!!!!!!
-		 */
-		table2.tableModel.addRow(new Object[]{"Requirement1", "Description1"});
-		table2.tableModel.addRow(new Object[]{"Requirement2", "Description2"});
-		table2.tableModel.addRow(new Object[]{"Requirement3", "Description3"});
-		table2.tableModel.addRow(new Object[]{"Requirement4", "Description4"});
-		table2.tableModel.addRow(new Object[]{"Requirement5", "Description5"});
-		table2.tableModel.addRow(new Object[]{"Requirement6", "Description6"});
-		table2.tableModel.addRow(new Object[]{"Requirement7", "Description7"});
-		table2.tableModel.addRow(new Object[]{"Requirement8", "Description8"});
-		table2.tableModel.addRow(new Object[]{"Requirement9", "Description9"});
-		table2.tableModel.addRow(new Object[]{"Requirement10", "Description10"});		
-		/*
-		 * Puts the table within a scroll pane, and adds to the view.
-		 */
-		JScrollPane tablePanel2 = new JScrollPane(table2);
-		tablePanel2.setMinimumSize(new Dimension(400, 100));
-		c.gridwidth = 8;
-		c.gridx = 0;
-		c.gridy = 1;
-		currentReqsPanel.add(tablePanel2, c);
-		
-		JButton removeReqButton = new JButton("Remove Selected Requirement");
-		c.gridx = 1;
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		c.gridy = 2;
-		c.ipady = 0;
-		c.ipadx = 0;
-		currentReqsPanel.add(removeReqButton, c);
-		
-		JButton removeAllReqButton = new JButton("Remove All Requirements");
-		c.gridx = 3;
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		c.gridy = 2;
-		currentReqsPanel.add(removeAllReqButton, c);
-		
-		JPanel blankPanel3 = new JPanel();
-		blankPanel3.setMinimumSize(new Dimension(400, 25));
-		c.gridx = 0;
-		c.gridy = 8;
-		c.gridwidth = 8;
-		rightPanel.add(blankPanel3, c);
-		
 		c.gridx = 0;
 		c.gridy = 0;
+		c.weighty = .5;
 		add(rightPanel, c);
+		
 		
 		JTextField errorLog = new JTextField("Any errors shown here.");
 		errorLog.setMinimumSize(new Dimension(150, 25));
