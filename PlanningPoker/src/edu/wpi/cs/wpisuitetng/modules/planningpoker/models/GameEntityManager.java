d9c2aca25d329f290b1d286b26ca80ed0e908745
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-04-01 05:11:42 -0400

    A bit more on the side of data validation in the database. This time for the Game Entity Manager

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/GameEntityManager.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/GameEntityManager.java
@@ -4,6 +4,7 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.models;
 
 import java.util.List;
+import java.util.UUID;
 
 import edu.wpi.cs.wpisuitetng.Session;
 import edu.wpi.cs.wpisuitetng.database.Data;
@@ -19,7 +20,9 @@ import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
 /**
  * 
- * Most of the code is referenced and refactored directly from the Requirement Entity Manager module
+ * Most of the code is referenced and refactored directly from the Requirement
+ * Entity Manager module
+ * 
  * @author dstapply
  * 
  */
@@ -34,42 +37,45 @@ public class GameEntityManager implements EntityManager<Game> {
 	@Override
 	public Game makeEntity(Session s, String content)
 			throws BadRequestException, ConflictException, WPISuiteException {
-		
+
 		// Parse game from JSON
 		final Game newGame = Game.fromJSON(content);
-		
-		// Save the game in the database if possible, otherwise, throw an exception
-		// We want the game to be associated with the project the user logged in to
+
+		// Save the game in the database if possible, otherwise, throw an
+		// exception
+		// We want the game to be associated with the project the user logged in
+		// to
 		if (!db.save(newGame, s.getProject())) {
 			throw new WPISuiteException();
 		}
-		
+
 		// Return the newly created game
 		return newGame;
 	}
 
 	@Override
-	public Game[] getEntity(Session s, String id) throws NotFoundException{
-		final int intId = Integer.parseInt(id);
-		if(intId < 1) {
+	public Game[] getEntity(Session s, String id) throws NotFoundException {
+		final UUID intId = UUID.fromString(id);
+		if (id == "") {
 			throw new NotFoundException();
 		}
 		Game[] games = null;
 		try {
-			games = db.retrieve(Game.class, "id", intId, s.getProject()).toArray(new Game[0]);
+			games = db.retrieve(Game.class, "identity", intId, s.getProject())
+					.toArray(new Game[0]);
 		} catch (WPISuiteException e) {
 			e.printStackTrace();
 		}
-		if(games.length < 1 || games[0] == null) {
+		if (games.length < 1 || games[0] == null) {
 			throw new NotFoundException();
 		}
 		return games;
-		
+
 	}
 
 	@Override
 	public Game[] getAll(Session s) throws WPISuiteException {
-		
+
 		// Ask the database to retrieve all objects of the type
 		// Game.
 		// Passing a dummy Game lets the db know what type of object
@@ -83,47 +89,56 @@ public class GameEntityManager implements EntityManager<Game> {
 
 	@Override
 	public Game update(Session s, String content) throws WPISuiteException {
-		// This module does not allow Games to be modified, so throw an exception
+		// This module does not allow Games to be modified, so throw an
+		// exception
 		Game updatedGame = Game.fromJSON(content);
-		
-		
-		List<Model> oldGame = db.retrieve(Game.class, "identity", updatedGame.getIdentifier(), s.getProject());
-		if(oldGame.size() < 1 || oldGame.get(0) == null){
+
+		List<Model> oldGame = db.retrieve(Game.class, "identity",
+				updatedGame.getIdentifier(), s.getProject());
+		if (oldGame.size() < 1 || oldGame.get(0) == null) {
 			throw new BadRequestException("Requirement with ID does not exist.");
 		}
-		
-		Game existingGame = (Game)oldGame.get(0);
+
+		Game existingGame = (Game) oldGame.get(0);
 		existingGame.copyFrom(updatedGame);
-		
-		if(!db.save(existingGame, s.getProject())){
+
+		if (!db.save(existingGame, s.getProject())) {
 			throw new WPISuiteException();
 		}
-		
+
 		return existingGame;
 	}
 
 	/**
 	 * Saves the game into the database for the current session
-	 * @see edu.wpi.cs.wpisuitetng.modules.EntityManager#save(edu.wpi.cs.wpisuitetng.Session, edu.wpi.cs.wpisuitetng.modules.Model)
+	 * 
+	 * @see edu.wpi.cs.wpisuitetng.modules.EntityManager#save(edu.wpi.cs.wpisuitetng.Session,
+	 *      edu.wpi.cs.wpisuitetng.modules.Model)
 	 */
 	@Override
-	public void save(Session s, Game model){
-	    // Save the given game in the database
+	public void save(Session s, Game model) {
+		// Save the given game in the database
 		db.save(model, s.getProject());
 	}
-	
+
 	/**
 	 * Deletes a requirement from the database
-	 * @param s the session
-	 * @param id the id of the requirement to delete
-
-	 * @return true if the deletion was successful * @throws WPISuiteException * @throws WPISuiteException * @throws WPISuiteException
-	 * @see edu.wpi.cs.wpisuitetng.modules.EntityManager#deleteEntity(Session, String) */
+	 * 
+	 * @param s
+	 *            the session
+	 * @param id
+	 *            the id of the requirement to delete
+	 * 
+	 * @return true if the deletion was successful * @throws WPISuiteException * @throws
+	 *         WPISuiteException * @throws WPISuiteException
+	 * @see edu.wpi.cs.wpisuitetng.modules.EntityManager#deleteEntity(Session,
+	 *      String)
+	 */
 
 	@Override
 	public boolean deleteEntity(Session s, String id) throws WPISuiteException {
 		ensureRole(s, Role.ADMIN);
-		return(db.delete(getEntity(s, id)[0])!= null) ? true: false;
+		return (db.delete(getEntity(s, id)[0]) != null) ? true : false;
 	}
 
 	@Override
@@ -142,7 +157,7 @@ public class GameEntityManager implements EntityManager<Game> {
 	@Override
 	public int Count() throws WPISuiteException {
 		// Return the number of Games currently in the database.
-	    return db.retrieveAll(new Game(null, null, null, true)).size();
+		return db.retrieveAll(new Game()).size();
 	}
 
 	@Override
@@ -158,16 +173,23 @@ public class GameEntityManager implements EntityManager<Game> {
 		// TODO Auto-generated method stub
 		throw new WPISuiteException();
 	}
-	
+
 	/**
 	 * Ensures that a user is of the specified role
-	 * @param session the session
-	 * @param role the role being verified
-	
-	 * @throws WPISuiteException user isn't authorized for the given role */
-	private void ensureRole(Session session, Role role) throws WPISuiteException {
-		User user = (User) db.retrieve(User.class, "username", session.getUsername()).get(0);
-		if(!user.getRole().equals(role)) {
+	 * 
+	 * @param session
+	 *            the session
+	 * @param role
+	 *            the role being verified
+	 * 
+	 * @throws WPISuiteException
+	 *             user isn't authorized for the given role
+	 */
+	private void ensureRole(Session session, Role role)
+			throws WPISuiteException {
+		User user = (User) db.retrieve(User.class, "username",
+				session.getUsername()).get(0);
+		if (!user.getRole().equals(role)) {
 			throw new UnauthorizedException();
 		}
 	}
