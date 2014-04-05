9b179a92fd4b94df88e57f59c7f64ae7fefc8a0c
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-04-05 17:50:41 -0400

    Added test cases for voting

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/vote/Vote.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/vote/Vote.java
@@ -2,17 +2,17 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.models.vote;
 
 public class Vote {
 
-	private String username;
+	private final String username;
 	private int voteNumber;
 	
+	/**
+	 * Constructor for a vote 
+	 * @param username The username of the voter
+	 * @param voteNumber The value of the vote
+	 */
 	public Vote(String username, int voteNumber){
 		this.username = username;
 		this.voteNumber = voteNumber;
-	}
-	
-	public Vote(){
-		this.username = null;
-		this.voteNumber = -1;
 	}
 
 	/**
---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/vote/VoteTest.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.models.vote;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VoteTest {
+	Vote aVote;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.aVote = new Vote("Steve", 37);
+	}
+
+	@Test
+	public void storageTest() {
+		assertEquals(aVote.getUsername(), "Steve");
+		assertEquals(aVote.getVoteNumber(), 37);
+	}
+
+}
