210b728a44ba67c50228fdc8970b2e9b089dacf0
jmwetzel <jmwetzel@wpi.edu>
2014-04-07 18:03:00 -0400

    Create a drop down box for hours, minutes, AM/PM jmwetzel AndrewBusch  On branch FeatureEndDate  Changes to be committed: 	modified:   PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/games/creation/AddEndDatePanel.java

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/games/creation/AddEndDatePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/games/creation/AddEndDatePanel.java
@@ -3,8 +3,11 @@
  */
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.games.creation;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+
 import java.util.Date;
+
 import org.jdesktop.swingx.JXDatePicker;
 
 /**
@@ -13,9 +16,19 @@ import org.jdesktop.swingx.JXDatePicker;
 public class AddEndDatePanel extends JPanel {
 	
 	private final JXDatePicker datePicker = new JXDatePicker(new Date());
+	private final String[] hourArray = 
+		{"1","2","3","4","5","6","7","8","9","10","11","12"} ;
+	private final String[] minuteArray = {"00","15","30","45"};
+	private final String[] AmPmArray = {"AM","PM"};
+	private final JComboBox hourSelection = new JComboBox(hourArray);
+	private final JComboBox minuteSelection = new JComboBox(minuteArray);
+	private final JComboBox AmPmSelection = new JComboBox(AmPmArray);
 	
 	public AddEndDatePanel(final CreateGamePanel view){
-		this.add(datePicker);	
+		this.add(datePicker);
+		this.add(hourSelection);
+		this.add(minuteSelection);
+		this.add(AmPmSelection);
 	}
 	
 	public Date getEndDate(){
