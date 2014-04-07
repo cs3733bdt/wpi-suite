94ee4e699a5d6e5c91e8e991fb8c89ddfe022473
Dalton Tapply <dstapply@wpi.edu>
2014-04-07 01:50:43 -0400

    Adds a test case for the the EmailNotification login method.

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/notification/testEmailNotification.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/notification/testEmailNotification.java
@@ -3,7 +3,13 @@
  */
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller.notification;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,9 +20,10 @@ import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Game;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.requirementmodels.Requirement;
 
 /**
- * @author alfeey44
+ * @author dstapply
  *
  */
+
 public class testEmailNotification {
 
 	Game g;
@@ -49,16 +56,48 @@ public class testEmailNotification {
 		en = new EmailNotification(g);
 	}
 	
+	/**
+	 * Best Test we can do, can't directly compare session objects because
+	 * getInstance will return different values for each session even
+	 * with the same username and password.
+	 */
+	@Test
+	public void testLogin() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		
+		// Initialize session
+		Session session;
+		
+		// Set session with authenticator
+		session = Session.getInstance(properties,
+				  new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("WPI.Suite.BDT.NoReply@gmail.com", "bobbydroptables");
+					}
+				  });
+		
+		assertEquals(session.getProperties(), en.login().getProperties());
+	}
+	
+	/**
+	 * Commended out to prevent spamming u1 with emails.
+	 * (Works as of 4/7/14)
+	 */
 	@Test
 	public void testSendEmail() {
-		en.sendEmail(en.login(), u1);
+		//en.sendEmail(en.login(), u1);
 	}
 
 	/**
 	 * Commented out to prevent spamming the alias.
+	 * (Works as of 4/7/14)
 	 */
 	@Test
 	public void testSendEmails() {
-		//en.sendEmails(g);
+		//en.sendEmails();
 	}
 }
