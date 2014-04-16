dc3c894a56dd8ce5bf8380f2a8ccf9c5e728be8d
dcuzunoglu <dcuzunoglu@wpi.edu>
2014-04-15 20:03:18 -0400

    finalizes design of lefthalf of active game panel, updates new active games panel and view event controller to include the new design Doruk Uzunoglu Jordan Wetzel

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/active/NewActiveGamePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/active/NewActiveGamePanel.java
@@ -12,39 +12,74 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.active;
 
 
 import java.awt.Dimension;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JSplitPane;
 
+import edu.wpi.cs.wpisuitetng.Session;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.game.Game;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.observers.AbstractModelObserver;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.observers.ObservableModel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.requirement.Requirement;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.games.creation.NewCreateGamePanel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.games.creation.NewLeftHalfCreateGamePanel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.games.creation.NewRightHalfCreateGamePanel;
 
 
-public class NewActiveGamePanel extends JSplitPane {
-	NewLeftHalfActiveGamePanel leftHalf = new NewLeftHalfActiveGamePanel();
-	NewRightHalfActiveGamePanel rightHalf = new NewRightHalfActiveGamePanel();
+public class NewActiveGamePanel extends JSplitPane implements AbstractModelObserver{
+	Game currentGame;
 	
-	public NewActiveGamePanel() {
-
+	
+	NewLeftHalfActiveGamePanel leftHalf;
+	NewRightHalfActiveGamePanel rightHalf; 
+	
+	public NewActiveGamePanel(final Game game) {
+		game.addObserver(this);	
+		currentGame = game;
+		
+		leftHalf = new NewLeftHalfActiveGamePanel(currentGame);
+		rightHalf = new NewRightHalfActiveGamePanel();
+		
 		this.setRightComponent(rightHalf);
-		//rightHalf.setMinimumSize(new Dimension(500, 400));
 		this.setLeftComponent(leftHalf);
 		this.setDividerLocation(400);
 		
 	}
 	
+	@Override
+	public void update(ObservableModel o, Object arg) {
+		if(o instanceof Game){
+			//TODO Handle an update to a model
+		}	
+	}
+	
+	public Game getGame() {
+		return currentGame;
+	}
+	
+	/*
 	public static void main(String args[]){
 		JFrame frame = new JFrame("Demo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
+        ArrayList<Requirement> reqs = new ArrayList<Requirement>();
+        reqs.add(new Requirement("test", "test"));
+        Game testGame;
+        testGame = new Game("Test Name", "Test Description", reqs, false, true);
+        testGame.setCreator("Doruk");
+        //User currentUser;
+        //currentUser = new User("Doruk","Doruk", "123", "blank", "blank", 5);
+        
         //Set up the content pane.
-        frame.add(new NewActiveGamePanel());
+        frame.add(new NewActiveGamePanel(testGame));
         frame.setMinimumSize(new Dimension(1000, 600));
 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
 	}
+*/
+	
 }
