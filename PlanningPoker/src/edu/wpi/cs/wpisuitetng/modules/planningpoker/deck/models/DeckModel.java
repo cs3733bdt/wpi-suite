47c463340822a77eda496ce673626814086827af
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-04-21 21:50:20 -0400

    Begins constructing the DeckModel class that will hold the decks for the current user

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/deck/models/DeckModel.java
@@ -0,0 +1,29 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.deck.models;
+
+import javax.swing.AbstractListModel;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.abstractmodel.AbstractModelObserver;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.abstractmodel.ObservableModel;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.game.models.Game;
+
+public class DeckModel extends AbstractListModel<Deck> implements AbstractModelObserver {
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Deck getElementAt(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(ObservableModel o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
