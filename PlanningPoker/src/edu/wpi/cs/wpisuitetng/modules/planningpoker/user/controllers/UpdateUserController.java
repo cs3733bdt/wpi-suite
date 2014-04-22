b1be7df8328f9f223a586d8ec6af33c460c531e9
Peter <paleondires@wpi.edu>
2014-04-22 04:34:56 -0400

    Defines update user controller and request observer. Not yet implemented.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/user/controllers/UpdateUserController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/user/controllers/UpdateUserController.java
@@ -1,5 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team Bobby Drop Tables
+ *******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.user.controllers;
+
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.user.observers.UpdateUserRequestObserver;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
 public class UpdateUserController {
 
+
+	private static UpdateUserController instance;
+	private UpdateUserRequestObserver observer;
+	
+	private UpdateUserController() {
+		observer = new UpdateUserRequestObserver(this);
+	}
+	
+	/**
+	 * @return a new instance of an UpdateGameController
+	 */
+	public static UpdateUserController getInstance() {
+		if(instance == null) {
+			instance = new UpdateUserController();
+		}
+		return instance;
+	}
+	
+	/**
+	 * updates a User with the same username?? on server
+	 * @param newUser User to be updated
+	 */
+	public void updateUser(User newUser) {
+		System.out.println("Updating " + newUser.getName() + " to server");
+		// Update request
+		Request request = Network.getInstance().makeRequest("core/user", HttpMethod.POST);
+		// Set the game to update
+		request.setBody(newUser.toJSON());
+		// Add observer to get response
+		request.addObserver(observer);
+		// Send the update request
+		request.send();
+	}
 }
+
+
--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/user/observers/UpdateUserRequestObserver.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/user/observers/UpdateUserRequestObserver.java
@@ -1,5 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team Bobby Drop Tables
+ *******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.user.observers;
 
-public class UpdateUserRequestObserver {
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.user.controllers.UpdateUserController;
+import edu.wpi.cs.wpisuitetng.network.RequestObserver;
+import edu.wpi.cs.wpisuitetng.network.models.IRequest;
+import edu.wpi.cs.wpisuitetng.network.models.ResponseModel;
 
+
+	public class UpdateUserRequestObserver implements RequestObserver {
+		/** We don't actually use the controller,
+		 * in the defect tracker they use it to print
+		 * error messages.
+		 */
+		private final UpdateUserController controller;
+		
+		/**
+		 * Constructs an observer for updating user controller
+		 * @param controller updateUserController to be observed
+		 */
+		public UpdateUserRequestObserver(UpdateUserController controller) {
+			this.controller = controller;
+		}
+		
+		/**
+		 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseSuccess(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+		 */
+		@Override
+		public void responseSuccess(IRequest iReq) {
+			final ResponseModel response = iReq.getResponse();
+			
+			// The user that got updated
+			User updatedUser = User.fromJSON(response.getBody());
+			System.out.println("Updated User:" + updatedUser);
+			
+			System.out.println("The request to update a user has succeeded!");
+		}
+		
+		/**
+		 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(edu.wpi.cs.wpisuitetng.network.models.IRequest)
+		 */
+		@Override
+		public void responseError(IRequest iReq) {
+			System.err.println("Response Error: " + iReq.getResponse().getStatusMessage());
+		}
+		
+		/**
+		 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(edu.wpi.cs.wpisuitetng.network.models.IRequest, java.lang.Exception)
+		 */
+		@Override
+		public void fail(IRequest iReq, Exception exception) {
+			System.err.println("The request to update a user failed with exception: "
+					+ exception.getMessage());
+		}
 }
+
