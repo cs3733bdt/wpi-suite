45869ff190bbcd61ca88a0d399479c8f950bb11c
Jeffrey Signore <jssignore@wpi.edu>
2014-04-24 00:11:06 -0400

    Fixes the layout of the Create Deck Panel: Adds the radio buttons for single v multiple selection. Anchors components to the left. Gets rid of set value textfield (this will be directly on each card that appears). Adds a save deck button. Jeffrey Signore

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/decks/creation/CreateDeckPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/decks/creation/CreateDeckPanel.java
@@ -11,14 +11,17 @@
 
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.decks.creation;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SpringLayout;
@@ -37,6 +40,11 @@ public class CreateDeckPanel extends JScrollPane {
 	
 	private NameJTextField numCards;
 	private NameJTextField cardValue;
+	
+	private JRadioButton singleSelection;
+	private JRadioButton multipleSelection;
+	
+	private JButton saveButton;
 	
 	public CreateDeckPanel(){
 		
@@ -65,30 +73,65 @@ public class CreateDeckPanel extends JScrollPane {
 		JScrollPane descriptionScroll = new JScrollPane(descriptionTextField);
 		descriptionScroll.setPreferredSize(new Dimension(400, 20));		
 		
-		JPanel numCardsAndColorPanel = new JPanel();
+		JPanel numCardsAndColorAndSelectedTypePanel = new JPanel();
+		
+		JLabel selectionTypeLabel = new JLabel("Selection Type * ");
+		JPanel selectionLabelPanel = new JPanel();
+		selectionLabelPanel.add(selectionTypeLabel);
+		singleSelection = new JRadioButton("Single Card Selection");
+		multipleSelection = new JRadioButton("Multiple Card Selection");
+		multipleSelection.setSelected(true);
+		ButtonGroup radioGroup = new ButtonGroup();						//Creates a radio button group to make it so that you can only select one of the radio buttons in the group
+		radioGroup.add(singleSelection);									//Adds the card option radio button to the group
+		radioGroup.add(multipleSelection);		
+		JPanel radioButtonsPanel = new JPanel();
+		radioButtonsPanel.setLayout(new BorderLayout());
+		radioButtonsPanel.add(selectionLabelPanel, BorderLayout.PAGE_START);
+		radioButtonsPanel.add(singleSelection, BorderLayout.LINE_START);
+		radioButtonsPanel.add(multipleSelection, BorderLayout.LINE_END);
+		
+		JPanel blankPanel1 = new JPanel();
+		blankPanel1.setPreferredSize(new Dimension(85, 5));
 		
 		JLabel numCardsLabel = new JLabel("Number of Cards * ");
+		JPanel numLabelPanel = new JPanel();
+		numLabelPanel.add(numCardsLabel);
 		numCards = new NameJTextField(5);
+		numCards.setText("1");
 		JButton submitNumCards = new JButton("Submit");
+		JPanel numPanel = new JPanel();
+		numPanel.setLayout(new BorderLayout());
+		numPanel.add(numLabelPanel, BorderLayout.PAGE_START);
+		numPanel.add(numCards, BorderLayout.LINE_START);
+		numPanel.add(submitNumCards, BorderLayout.LINE_END);
 		
-		JPanel blankPanel = new JPanel();
-		blankPanel.setPreferredSize(new Dimension(90,5));
+		JPanel blankPanel2 = new JPanel();
+		blankPanel2.setPreferredSize(new Dimension(95, 5));
 		
 		JLabel colorLabel = new JLabel("Select Card Color * ");
-		JComboBox<String> colorDropDown = new JComboBox<String>();
-		
+		JPanel colorLabelPanel = new JPanel();
+		colorLabelPanel.add(colorLabel);
+		JComboBox<String> colorDropDown = new JComboBox<String>();	
 		colorDropDown.addItem("Red (Default)");
 		colorDropDown.addItem("Blue");
 		colorDropDown.addItem("Green");
 		colorDropDown.addItem("Purple");
 		colorDropDown.addItem("Yellow");
+		JPanel colorPanel = new JPanel();
+		colorPanel.setLayout(new BorderLayout());
+		colorPanel.add(colorLabelPanel, BorderLayout.PAGE_START);
+		colorPanel.add(colorDropDown, BorderLayout.PAGE_END);
 		
-		numCardsAndColorPanel.add(numCardsLabel);
-		numCardsAndColorPanel.add(numCards);
-		numCardsAndColorPanel.add(submitNumCards);
-		numCardsAndColorPanel.add(blankPanel);
-		numCardsAndColorPanel.add(colorLabel);
-		numCardsAndColorPanel.add(colorDropDown);
+		numCardsAndColorAndSelectedTypePanel.add(radioButtonsPanel);
+		numCardsAndColorAndSelectedTypePanel.add(blankPanel1);
+		numCardsAndColorAndSelectedTypePanel.add(numPanel);
+		/*numCardsAndColorAndSelectedTypePanel.add(numCardsLabel);
+		numCardsAndColorAndSelectedTypePanel.add(numCards);
+		numCardsAndColorAndSelectedTypePanel.add(submitNumCards);*/
+		numCardsAndColorAndSelectedTypePanel.add(blankPanel2);
+		/*numCardsAndColorAndSelectedTypePanel.add(colorLabel);
+		numCardsAndColorAndSelectedTypePanel.add(colorDropDown);*/
+		numCardsAndColorAndSelectedTypePanel.add(colorPanel);
 		
 		JPanel cardsPanel = new JPanel();
 		cardsPanel.setPreferredSize(new Dimension(10, 400));
@@ -100,20 +143,22 @@ public class CreateDeckPanel extends JScrollPane {
 		valuePanel.add(cardValue);
 		valuePanel.add(setCardValue);
 		
+		saveButton = new JButton("Save Deck");
+		
 		/* Add components to the container */
 		view.add(nameLabel);
 		view.add(nameTextField);
 		view.add(descriptionLabel);
 		view.add(descriptionScroll);
-		view.add(numCardsAndColorPanel);
+		view.add(numCardsAndColorAndSelectedTypePanel);
 		/*view.add(numCardsLabel);
 		view.add(numCards);
 		view.add(submitNumCards);
 		view.add(colorLabel);
 		view.add(colorDropDown);*/
 		view.add(cardsPanel);
-		view.add(valuePanel);
-		
+		//view.add(valuePanel);
+		view.add(saveButton);
 		
 		/* Sets the layout constraints for each component */
 		layout.putConstraint(SpringLayout.WEST, nameLabel, 10, SpringLayout.WEST, view);
@@ -127,10 +172,10 @@ public class CreateDeckPanel extends JScrollPane {
 		
 		layout.putConstraint(SpringLayout.WEST, descriptionScroll, 5, SpringLayout.EAST, descriptionLabel);
 		layout.putConstraint(SpringLayout.NORTH, descriptionScroll, 10, SpringLayout.NORTH, view);
-		layout.putConstraint(SpringLayout.EAST, descriptionScroll, -10, SpringLayout.EAST, view);
+		//layout.putConstraint(SpringLayout.EAST, descriptionScroll, -10, SpringLayout.EAST, view);
 		
-		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, numCardsAndColorPanel, 5, SpringLayout.HORIZONTAL_CENTER, view);
-		layout.putConstraint(SpringLayout.NORTH, numCardsAndColorPanel, 10, SpringLayout.SOUTH, nameLabel);
+		layout.putConstraint(SpringLayout.WEST, numCardsAndColorAndSelectedTypePanel, 0, SpringLayout.WEST, view);
+		layout.putConstraint(SpringLayout.NORTH, numCardsAndColorAndSelectedTypePanel, 10, SpringLayout.SOUTH, nameLabel);
 		
 		/*layout.putConstraint(SpringLayout.WEST, numCardsLabel, 10, SpringLayout.WEST, view);
 		layout.putConstraint(SpringLayout.NORTH, numCardsLabel, 12, SpringLayout.SOUTH, nameTextField);
@@ -149,12 +194,15 @@ public class CreateDeckPanel extends JScrollPane {
 		*/
 		layout.putConstraint(SpringLayout.WEST, cardsPanel, 10, SpringLayout.WEST, view);
 		layout.putConstraint(SpringLayout.EAST, cardsPanel, -10, SpringLayout.EAST, view);
-		layout.putConstraint(SpringLayout.NORTH, cardsPanel, 10, SpringLayout.SOUTH, numCardsAndColorPanel);
-		layout.putConstraint(SpringLayout.SOUTH, cardsPanel, -100, SpringLayout.SOUTH, view);
+		layout.putConstraint(SpringLayout.NORTH, cardsPanel, 10, SpringLayout.SOUTH, numCardsAndColorAndSelectedTypePanel);
+		layout.putConstraint(SpringLayout.SOUTH, cardsPanel, -45, SpringLayout.SOUTH, view);
 		
-		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, valuePanel, 5, SpringLayout.HORIZONTAL_CENTER, view);
+		layout.putConstraint(SpringLayout.WEST, saveButton, 10, SpringLayout.WEST, view);
+		layout.putConstraint(SpringLayout.NORTH, saveButton, 10, SpringLayout.SOUTH, cardsPanel);
+				
+		/*layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, valuePanel, 5, SpringLayout.HORIZONTAL_CENTER, view);
 		layout.putConstraint(SpringLayout.NORTH, valuePanel, 10, SpringLayout.SOUTH, cardsPanel);
-		
+		*/
 		
 		/*layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, colorLabel, 5, SpringLayout.HORIZONTAL_CENTER, view);
 		layout.putConstraint(SpringLayout.NORTH, colorLabel, 75, SpringLayout.SOUTH, nameLabel);
