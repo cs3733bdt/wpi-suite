a0657692017d1cf6e21a6a9ce4323b5c2b1e2b55
dcuzunoglu <dcuzunoglu@wpi.edu>
2014-04-26 23:34:15 -0400

    adds tests DescriptionJTextFieldArea class.

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/components/DescriptionJTextFieldAreaTest.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team Bobby Drop Tables
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.components;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DescriptionJTextFieldAreaTest {
+	private DescriptionJTextArea textArea;
+	private DescriptionJTextArea blankTextArea;
+	private ErrorLabel errorLabel;
+	
+	@Before
+	public void setUp() {
+		textArea = new DescriptionJTextArea("Test Text Area");
+		textArea.displayStandardBorder();
+		blankTextArea = new DescriptionJTextArea();
+		errorLabel = new ErrorLabel();
+	}
+	
+	@Test
+	public void setTextTest() {
+		textArea.setText("Bobby Drop Tables");
+		assertEquals("Bobby Drop Tables", textArea.getText());
+	}
+	
+	@Test
+	public void hasChangesTest() {
+		assertTrue(textArea.hasChanges());
+	}
+	
+	@Test
+	public void validateFieldTest() {
+		assertTrue(textArea.validateField(errorLabel, false, false));
+		assertFalse(blankTextArea.validateField(errorLabel, true, false));
+		assertFalse(blankTextArea.validateField(errorLabel, false, true));
+		assertFalse(blankTextArea.validateField(errorLabel, true, true));
+		
+	}
+	
+}
