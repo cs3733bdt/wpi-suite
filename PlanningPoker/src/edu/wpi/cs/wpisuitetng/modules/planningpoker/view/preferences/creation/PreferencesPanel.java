a1b6d0eb788c9c54c997bede94fa795077e8549b
Peter <paleondires@wpi.edu>
2014-04-28 17:36:25 -0400

    Lowers the sleep in the preferences panel (needs time to insure we can get the current user data from server)

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/preferences/creation/PreferencesPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/preferences/creation/PreferencesPanel.java
@@ -600,7 +600,7 @@ public class PreferencesPanel extends JScrollPane implements IDataField {
 			}
 		}catch(NullPointerException e){
 			try {
-				Thread.sleep(2000);
+				Thread.sleep(500);
 			} catch (InterruptedException e1) {
 				// TODO Auto-generated catch block
 				//e1.printStackTrace();
