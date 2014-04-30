833f51621a73357399500672968e26c6bf9fd5c4
jdcanas <justincanas16@gmail.com>
2014-04-30 03:30:13 -0400

    Added arrow dropdown skeleton. Justin Canas

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/help/ArrowDropDown.java
@@ -0,0 +1,15 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.help;
+
+
+//TODO implement a list of components associated with this arrow
+
+public class ArrowDropDown {
+  public ArrowDropDown() {
+	  
+  }
+  
+//  public void addComponent(JComponent comp) {
+//	  //TODO add component to list holding components associated with this arrow
+//  }
+  
+}
