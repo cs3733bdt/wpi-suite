1254938f951fad9bda7763b981f9003e996b631a
Jake <jimcsweeney@wpi.edu>
2014-05-01 18:48:23 -0400

    added the help window to create deck

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/help/CreateDeckHelp.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/help/CreateDeckHelp.java
@@ -12,6 +12,7 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.help;
 
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
 import java.io.IOException;
@@ -29,7 +30,7 @@ public class CreateDeckHelp extends JScrollPane implements IHelpPanel {
 	JLabel setUpCardspic;
 	JTextArea setUpCardsExplanation;
 	JLabel addingValuesToTheCardsHeading;
-	JLabel saddingValuesToTheCardspic;
+	JLabel addingValuesToTheCardspic;
 	JTextArea addingValuesToTheCardsExplanation;
 	
 	public CreateDeckHelp() {
@@ -42,12 +43,12 @@ public class CreateDeckHelp extends JScrollPane implements IHelpPanel {
 		Container view = new Container();
 		SpringLayout layout = new SpringLayout();
 		view.setLayout(layout);
+		view.setPreferredSize(new Dimension(610, 700));
 		
 		//Add the heading label to the Panel
 		headingLabel = new JLabel("Create Deck Help");
 		headingLabel.setFont(makeFont(8));
-		view.add(headingLabel);
-		
+			
 		
 		//Add label to explanation for initalizing the cards
 		setUpCardsHeading = new JLabel("Initialize the Deck");
@@ -56,21 +57,75 @@ public class CreateDeckHelp extends JScrollPane implements IHelpPanel {
 		//Add deck set up pic to the image panel;
 		setUpCardspic = addImage("set_up_cards.png");
 
-		//Add explanation for the first image, the email help
+		//Add explanation for setting up cards
 		setUpCardsExplanation = new JTextArea();
-		setUpCardsExplanation.setText("");
+		setUpCardsExplanation.setText("To start making a deck, you firstr have to enter the Name of the deck "
+				+ "you want created, and optionaly, you can enter a Discription of that deck.  The selection Type allows "
+				+ "you to be able to select either multiple cards or just a single card when voting, selecting multiple cards "
+				+ "adds up all of the cards that u select and puts that as your total.  The next step is to select the muber of cards that you want to be in your deck,"
+				+ "and then it gives you the option of changing the color of your cards, the default color is red.  "
+				+ "Finally you can choose whether or not to have an 'I dont know' button");
 
 		setUpCardsExplanation.setEditable(false);
 		setUpCardsExplanation.setBackground(null);
 		setUpCardsExplanation.setWrapStyleWord(true);
 		setUpCardsExplanation.setLineWrap(true);
 
+		//Add label to explanation for adding values to cards
+		addingValuesToTheCardsHeading = new JLabel("Initialize the Deck");
+		addingValuesToTheCardsHeading.setFont(makeFont(5));
+
+		//Add value pic to the image panel;
+		addingValuesToTheCardspic = addImage("adding_Values_To_The_Cards.png");
+
+		//Add explanation for adding values to the cards
+		addingValuesToTheCardsExplanation = new JTextArea();
+		addingValuesToTheCardsExplanation.setText("After the initialization of the deck you will se the cards appear"
+				+ " in the open area of the view.  To add a value to a card select the text field that is on the card"
+				+ "and enter the desired value for that card.  To set this value to the card all you have to do is "
+				+ "unselect the card");
+
+		addingValuesToTheCardsExplanation.setEditable(false);
+		addingValuesToTheCardsExplanation.setBackground(null);
+		addingValuesToTheCardsExplanation.setWrapStyleWord(true);
+		addingValuesToTheCardsExplanation.setLineWrap(true);
+		
+		view.add(headingLabel);
+		view.add(setUpCardsHeading);
+		view.add(setUpCardspic);
+		view.add(setUpCardsExplanation);
+		view.add(addingValuesToTheCardsHeading);
+		view.add(addingValuesToTheCardspic);
+		view.add(addingValuesToTheCardsExplanation);
 		
 		/**
 		 * Constraints for the overall panel layout
 		 */
 		layout.putConstraint(SpringLayout.NORTH, headingLabel, 5, SpringLayout.NORTH, view);
 		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, headingLabel, 0, SpringLayout.HORIZONTAL_CENTER, view);
+		
+		// initialize the deck
+		layout.putConstraint(SpringLayout.NORTH, setUpCardsHeading, 5, SpringLayout.SOUTH, headingLabel);
+		layout.putConstraint(SpringLayout.WEST, setUpCardsHeading, 20, SpringLayout.WEST, view);
+		
+		layout.putConstraint(SpringLayout.NORTH, setUpCardspic, 20, SpringLayout.SOUTH, setUpCardsHeading);
+		layout.putConstraint(SpringLayout.WEST, setUpCardspic, 5, SpringLayout.WEST, view);
+		
+		layout.putConstraint(SpringLayout.NORTH, setUpCardsExplanation, 20, SpringLayout.SOUTH, setUpCardsHeading);
+		layout.putConstraint(SpringLayout.EAST, setUpCardsExplanation, 0, SpringLayout.EAST, view);
+		layout.putConstraint(SpringLayout.WEST, setUpCardsExplanation, 20, SpringLayout.EAST, setUpCardspic);
+
+		// adding values to the cards
+		layout.putConstraint(SpringLayout.NORTH, addingValuesToTheCardsHeading, 5, SpringLayout.SOUTH, setUpCardspic);
+		layout.putConstraint(SpringLayout.WEST, addingValuesToTheCardsHeading, 20, SpringLayout.WEST, view);
+		
+		layout.putConstraint(SpringLayout.NORTH, addingValuesToTheCardspic, 20, SpringLayout.SOUTH, addingValuesToTheCardsHeading);
+		layout.putConstraint(SpringLayout.WEST, addingValuesToTheCardspic, 5, SpringLayout.WEST, view);
+		
+		layout.putConstraint(SpringLayout.NORTH, addingValuesToTheCardsExplanation, 20, SpringLayout.SOUTH, addingValuesToTheCardsHeading);
+		layout.putConstraint(SpringLayout.EAST, addingValuesToTheCardsExplanation, 0, SpringLayout.EAST, view);
+		layout.putConstraint(SpringLayout.WEST, addingValuesToTheCardsExplanation, 20, SpringLayout.EAST, addingValuesToTheCardspic);
+
 		setViewportView(view);
 		revalidate();
 		repaint();
@@ -117,6 +172,5 @@ public class CreateDeckHelp extends JScrollPane implements IHelpPanel {
 		return helpLabel;
 		
 	}
-
-}
+	}
 
