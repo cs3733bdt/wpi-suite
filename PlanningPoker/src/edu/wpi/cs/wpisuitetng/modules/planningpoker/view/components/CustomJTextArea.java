3846aa2cb0e4d6eb6ba8a4b3f241d8f41feae0a5
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2014-05-01 18:38:56 -0400

    Adds two custom JText classes that both implement the ability to enable select all text on mouse listener

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/components/CustomJTextArea.java
@@ -0,0 +1,30 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.components;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTextArea;
+import javax.swing.text.Document;
+
+public abstract class CustomJTextArea extends JTextArea implements IDataField {
+
+	/** @see JTextArea#JTextArea()*/
+	protected CustomJTextArea(){}
+	/** @see JTextArea#JTextArea(String) */
+	protected CustomJTextArea(String text) { super(text); }
+	
+	/**
+	 * Mouse listener for number of cards textfield. When the text field is
+	 * clicked, it selects all of the text so that the user can easily overwrite
+	 * their previous number.
+	 */
+	public void enableSelectAllTextOnMouseListener(){
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				selectAll();
+			}
+		});
+	}
+
+}
