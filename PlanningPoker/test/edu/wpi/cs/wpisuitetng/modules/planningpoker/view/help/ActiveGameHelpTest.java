9eea7ff365bf47580c29ee3549d2c887f45f9f6b
Peter <paleondires@wpi.edu>
2014-05-01 18:09:28 -0400

    Adds testing and additional comments for the help panels.

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/help/ActiveGameHelpTest.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Team Bobby Drop Tables
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.help;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class ActiveGameHelpTest {
+
+	ActiveGameHelp panel = new ActiveGameHelp();
+	
+	
+	@Test
+	public void readyToRemoveTest(){
+		assertTrue(panel.readyToRemove());
+	}
+	
+	@Test
+	public void getIdentifierIndexTest(){
+		assertEquals(2, panel.getIdentifierIndex());
+	}
+	/*
+	@Test
+	public void addImageTest(){
+
+	} */
+}
+
