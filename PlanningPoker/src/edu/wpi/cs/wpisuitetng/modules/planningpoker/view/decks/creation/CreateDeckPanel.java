45e5331e240a0f2d2a8cc0904932aa9ccdfb0ebb
Jeffrey Signore <jssignore@wpi.edu>
2014-05-01 21:58:21 -0400

    Changes all instances of 24 to 15, because the new maximum number of cards allowed is 15 [suggestion by Mr. Weasel]. Jeffrey Signore

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/decks/creation/CreateDeckPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/decks/creation/CreateDeckPanel.java
@@ -265,7 +265,7 @@ public class CreateDeckPanel extends JScrollPane implements IDataField,
 		numCards = new NumberJTextField(10);
 		numCards.setPreferredSize(new Dimension(40,22));
 		numCards.setText("1");
-		numCards.setMaxValue(24);
+		numCards.setMaxValue(15);
 		numCards.addKeyListener(this);
 		//initializeArrayList();
 		//Selection mode is now enabled by default
@@ -570,15 +570,14 @@ public class CreateDeckPanel extends JScrollPane implements IDataField,
 	}
 
 	/**
-	 * Checks to make sure the number of cards inputted is 1-24
+	 * Checks to make sure the number of cards inputted is 1-15
 	 * 
 	 * @return true if a valid input
 	 */
 	private boolean verifyNumberOfCards() {
 		String text = numCards.getText();
 		String allowedChars = "123456789";
-		String allowedChars1 = "01234";
-		String allowedChars2 = "0123456789";
+		String allowedChars1 = "012345";
 		if (text.length() == 0) {
 			return false;
 		}
@@ -588,18 +587,14 @@ public class CreateDeckPanel extends JScrollPane implements IDataField,
 				return true;
 			}
 		}
-		/* Checks to see if the number is 10-24 */
+		/* Checks to see if the number is 10-15 */
 		if (text.length() == 2) {
-			if (text.charAt(0) == '2') {
+			if (text.charAt(0) == '1') {
 				if (allowedChars1.contains(Character.toString(text.charAt(1)))) {
 					return true;
 				}
 			}
-			if (text.charAt(0) == '1') {
-				if (allowedChars2.contains(Character.toString(text.charAt(1)))) {
-					return true;
-				}
-			}
+			/* Checks to see if the number is 01-09 */
 			if (text.charAt(0) == '0') {
 				if (allowedChars.contains(Character.toString(text.charAt(1)))) {
 					return true;
@@ -690,7 +685,7 @@ public class CreateDeckPanel extends JScrollPane implements IDataField,
 			public void mouseClicked(MouseEvent arg0) {
 				if (!submitNumCards.isEnabled()) {
 					numCards.validateField(errorField, true, true);
-					//errorField.setText("Number of cards must be a 1-or-2-digit integer between 1 and 24");
+					//errorField.setText("Number of cards must be a 1-or-2-digit integer between 1 and 15");
 				} else {
 				}// it will perform the action listener
 			}
