45a29fbbbea0e80f52969e7ba04df14df30d15e4
TomPaolillo <tjp560@gmail.com>
2014-05-02 19:21:47 -0400

    fixes update button validation

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/games/creation/RightHalfCreateGamePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/games/creation/RightHalfCreateGamePanel.java
@@ -896,7 +896,7 @@ public class RightHalfCreateGamePanel extends JScrollPane implements IDataField 
 		if (checkduplicateReq(new PPRequirement(nameArea.getText(),
 				descArea.getText()))) {
 			uniqueName = false;
-			displayError("A requirement already exists with that name");
+			displayError("A requirement already exists with that name and description");
 		} else {
 			displayError("");
 			uniqueName = true;
@@ -1115,75 +1115,16 @@ public class RightHalfCreateGamePanel extends JScrollPane implements IDataField 
 	}
 
 	private void updateUpdateButton() {
-		if (validateNameAndDesc(false, true) && updateValid()) {// TODO figure
-																// out these
-																// branches. I
-																// don't think
-																// validateNameAndDesc
-																// is necessary
-																// here
-
-			System.out.println("First branch of updateupdate");
+		if (validateNameAndDesc(true, false) && updateValid()) {
 			updateAddReqButton.setEnabled(true);
 			displayError("");
 		} else if (!updateValid()) {
-			System.out.println("Second branch of updateupdate");
 			updateAddReqButton.setEnabled(false);
 			displayError("No changes have been made");
 		} else {
-			System.out.println("Third branch of updateupdate");
 			updateAddReqButton.setEnabled(false);
-			if (updateValid() && validateNameAndDescForUpdate(false, true)) {
-				if (nameArea.getText().equals(
-						(String) currentTable.getValueAt(globalRow, 0))) {
-					updateAddReqButton.setEnabled(true);
-					displayError("");
-				}
-			}
+			displayError("A requirement already exists with that name and description");
 		}
-	}
-
-	/**
-	 * validates the name and description when a requirement is being edited.
-	 * This is called in the updateupdatebutton function
-	 * 
-	 * @param showLabel
-	 *            show the error label with appropriate message
-	 * @param showBox
-	 *            show the error box around the appropriate field
-	 * @return
-	 */
-	private boolean validateNameAndDescForUpdate(boolean showLabel,
-			boolean showBox) {
-		boolean returnStatus = true;
-		if (descArea.getText().isEmpty()) {
-			if (showLabel) {
-				displayError("Description is required");
-			}
-			if (showBox) {
-				descArea.setBorder(errorBorder);
-			}
-			returnStatus = false;
-		}
-
-		if (nameArea.getText().isEmpty()) {
-			if (showLabel) {
-				displayError("Name is required");
-			}
-			if (showBox) {
-				nameArea.setBorder(errorBorder);
-			}
-			returnStatus = false;
-		}
-
-		if (returnStatus) {
-			nameArea.setBorder(defaultTextFieldBorder);
-			descArea.setBorder(defaultTextAreaBorder);
-		}
-
-		nameArea.validateField(errorLabel, showLabel, showBox);
-
-		return returnStatus;
 	}
 
 	private void updateSubmitButton() {
@@ -1202,8 +1143,7 @@ public class RightHalfCreateGamePanel extends JScrollPane implements IDataField 
 		String currentName = (String) currentTable.getValueAt(globalRow, 0);
 		String updateDesc = descArea.getText();
 		String currentDesc = (String) currentTable.getValueAt(globalRow, 1);
-		return (!(currentName.equals(updateName)))
-				|| (!(currentDesc.equals(updateDesc)));
+		return (!(currentName.equals(updateName)))|| (!(currentDesc.equals(updateDesc)));
 	}
 
 	public boolean isNameAreaEmpty() {
