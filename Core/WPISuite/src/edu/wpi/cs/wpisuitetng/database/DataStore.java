c7ee09b722de86e361acd6a1e6098ee6665fdcef
AndrewBusch <andrew.b.busch@gmail.com>
2014-05-03 14:46:57 -0400

    change WPI-suite for deploying iteration 6 progress to tiki AndrewBusch

--- Core/WPISuite/src/edu/wpi/cs/wpisuitetng/database/DataStore.java
+++ Core/WPISuite/src/edu/wpi/cs/wpisuitetng/database/DataStore.java
@@ -34,7 +34,7 @@ import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
 public class DataStore implements Data {
 
-	static String WPI_TNG_DB ="Team2Iteration4";
+	static String WPI_TNG_DB ="Team2Iteration6";
 	private static DataStore myself = null;
 	static ObjectContainer theDB;
 	static ObjectServer server;
